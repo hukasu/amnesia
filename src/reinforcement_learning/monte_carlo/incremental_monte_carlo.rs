e981155d4f15878b771238e3ab2effeefd104e45
Lucas Farias <lucasfnf@gmail.com>
2023-11-08 19:31:30 +0900

    Refactor `policy_search` of `IncrementalMonteCarlo`

--- src/reinforcement_learning/monte_carlo/incremental_monte_carlo.rs
+++ src/reinforcement_learning/monte_carlo/incremental_monte_carlo.rs
@@ -1,14 +1,13 @@
-use std::collections::VecDeque;
 use std::marker::PhantomData;
 
 use crate::{
-    action::DiscreteAction, agent::Agent, environment::EpisodicEnvironment,
+    action::DiscreteAction,
+    agent::Agent,
+    environment::EpisodicEnvironment,
     observation::DiscreteObservation,
+    reinforcement_learning::{monte_carlo::MonteCarlo, PolicyEstimator},
+    trajectory::Trajectory,
 };
-
-use crate::reinforcement_learning::PolicyEstimator;
-
-use super::MonteCarlo;
 
 pub struct IncrementalMonteCarlo<E: EpisodicEnvironment> {
     return_discount: f64,
@@ -43,51 +42,32 @@ impl<
     type Environment = E;
 
     fn policy_search(self, environment: &mut Self::Environment, agent: &mut E::Agent) {
-        let mut visit_count = vec![0usize; S::OBSERVATIONS.len() * AC::ACTIONS.len()];
-        let mut observation_values = vec![0.0f64; S::OBSERVATIONS.len() * AC::ACTIONS.len()];
-        let mut trajectory = vec![];
-        let mut episode_returns = vec![];
+        let step_update = |step: &Trajectory<S, AC>,
+                           step_return: &f64,
+                           _visited: &mut [bool],
+                           visit_count: &mut [usize],
+                           _total_returns: &mut [f64],
+                           observation_values: &mut [f64]|
+         -> f64 {
+            let markov_reward_process_index =
+                Self::markov_reward_process_observation_action_pair_index(step);
+            visit_count[markov_reward_process_index] += 1;
 
-        let mut episode = 0usize;
-        let mut episode_variation_window = VecDeque::from_iter([f64::MAX; 5]);
-        while episode_variation_window
-            .iter()
-            .any(|ep_v| ep_v > &f64::EPSILON)
-            && episode < self.episodes
-        {
-            episode += 1;
-            let mut episode_variation = 0.;
+            let old_observation_value = observation_values[markov_reward_process_index];
+            let alpha = 1. / visit_count[markov_reward_process_index] as f64;
+            observation_values[markov_reward_process_index] = observation_values
+                [markov_reward_process_index]
+                + alpha * (step_return - observation_values[markov_reward_process_index]);
+            (old_observation_value - observation_values[markov_reward_process_index]).powi(2)
+        };
 
-            Self::generate_trajectory(environment, agent, &mut trajectory);
-            Self::discounted_return(&trajectory, self.return_discount, &mut episode_returns);
-
-            for (step, g_t) in trajectory.iter().zip(episode_returns.iter()) {
-                let markov_reward_process_index =
-                    Self::markov_reward_process_observation_action_pair_index(step);
-                visit_count[markov_reward_process_index] += 1;
-
-                let old_observation_value = observation_values[markov_reward_process_index];
-                let alpha = 1. / visit_count[markov_reward_process_index] as f64;
-                observation_values[markov_reward_process_index] = observation_values
-                    [markov_reward_process_index]
-                    + alpha * (g_t - observation_values[markov_reward_process_index]);
-                episode_variation += (old_observation_value
-                    - observation_values[markov_reward_process_index])
-                    .powi(2);
-            }
-            episode_variation_window.pop_front();
-            episode_variation_window.push_back(episode_variation);
-
-            let value_function = Self::make_value_fuction(&observation_values);
-            agent.policy_improvemnt(value_function);
-        }
-
-        Self::print_observation_action_pairs(
-            "Observation Visit Count",
-            &visit_count.iter().map(|u| *u as f64).collect::<Vec<_>>(),
-        );
-        Self::print_observation_action_pairs("Action Value Function", &observation_values);
-        println!("Converged in {} episodes.", episode);
+        self.monte_carlo_policy_search(
+            environment,
+            agent,
+            self.return_discount,
+            self.episodes,
+            &step_update,
+        )
     }
 }
 
